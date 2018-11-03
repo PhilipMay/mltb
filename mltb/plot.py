cebdf0597562a47448ef9a0b60430eb5be8820b1
Philip May <eniak.info@gmail.com>
2018-11-03 22:27:54 +0100

    can set title in twin_axes_plot

--- mltb/plot.py
+++ mltb/plot.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot
 
 # see https://matplotlib.org/api/_as_gen/matplotlib.axes.Axes.twinx.html
-def twin_axes_plot(values_1, label_1, values_2, label_2, shift_1=0, shift_2=0, label_x = 'x', color_1 = 'tab:red', color_2 = 'tab:blue'):
+def twin_axes_plot(values_1, label_1, values_2, label_2, shift_1=0, shift_2=0, title=None, label_x = 'x', color_1 = 'tab:red', color_2 = 'tab:blue'):
     """Create twin axes plot."""
     fig, ax1 = matplotlib.pyplot.subplots()
     
+    if title != None:
+        matplotlib.pyplot.title(title)
+
     ax1.set_xlabel(label_x)
 
     ax1.set_ylabel(label_1, color=color_1)
@@ -20,5 +23,5 @@ def twin_axes_plot(values_1, label_1, values_2, label_2, shift_1=0, shift_2=0, l
     # otherwise the labels might be slightly clipped
     # see https://matplotlib.org/users/tight_layout_guide.html
     fig.tight_layout() 
-    
+
     matplotlib.pyplot.show()
