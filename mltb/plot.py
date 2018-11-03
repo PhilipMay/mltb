4008f1d958e1d2dcd5c87dbe408336c2aa485fa7
Philip May <eniak.info@gmail.com>
2018-11-03 22:14:21 +0100

    twin_axes_plot shift and small renaming

--- mltb/plot.py
+++ mltb/plot.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot
 
 # see https://matplotlib.org/api/_as_gen/matplotlib.axes.Axes.twinx.html
-def twin_axes_plot(val_1, label_1, val_2, label_2, label_x = 'x', color_1 = 'tab:red', color_2 = 'tab:blue'):
+def twin_axes_plot(values_1, label_1, values_2, label_2, shift_1=0, shift_2=0, label_x = 'x', color_1 = 'tab:red', color_2 = 'tab:blue'):
     """Create twin axes plot."""
     fig, ax1 = matplotlib.pyplot.subplots()
     
     ax1.set_xlabel(label_x)
 
     ax1.set_ylabel(label_1, color=color_1)
-    ax1.plot(range(len(val_1)), val_1, color=color_1)
+    ax1.plot(range(shift_1, len(values_1) + shift_1), values_1, color=color_1)
     ax1.tick_params(axis='y', labelcolor=color_1)
 
     ax2 = ax1.twinx()
     
     ax2.set_ylabel(label_2, color=color_2)
-    ax2.plot(range(len(val_2)), val_2, color=color_2)
+    ax2.plot(range(shift_2, len(values_2) + shift_2), values_2, color=color_2)
     ax2.tick_params(axis='y', labelcolor=color_2)
 
     # otherwise the labels might be slightly clipped
