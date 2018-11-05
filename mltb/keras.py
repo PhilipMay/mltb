0822c8d77c8a9714890886ee6187d062f27cc3c4
Philip May <eniak.info@gmail.com>
2018-11-05 17:48:39 +0100

    attribute refactoring

--- mltb/keras.py
+++ mltb/keras.py
@@ -23,7 +23,7 @@ class BinaryClassifierMetricsCallback(keras.callbacks.Callback):
         self.val_data = val_data
         self.val_labels = val_labels
         self.pos_label = pos_label
-        self.hist = {}
+        self.metrics = {}
     
     def on_epoch_end(self, batch, logs={}):       
         predict_results = self.model.predict(self.val_data)
@@ -31,14 +31,14 @@ class BinaryClassifierMetricsCallback(keras.callbacks.Callback):
         round_predict_results = numpy.rint(predict_results)
         
         roc_auc = sklearn.metrics.roc_auc_score(self.val_labels, predict_results)
-        tools.append_to_dict(self.hist, "roc_auc", roc_auc)
+        tools.append_to_dict(self.metrics, "roc_auc", roc_auc)
         
         f1 = sklearn.metrics.f1_score(self.val_labels, round_predict_results)
-        tools.append_to_dict(self.hist, "f1", f1)
+        tools.append_to_dict(self.metrics, "f1", f1)
 
         accuracy = sklearn.metrics.accuracy_score(self.val_labels, round_predict_results)
-        tools.append_to_dict(self.hist, "accuracy", accuracy)
+        tools.append_to_dict(self.metrics, "accuracy", accuracy)
 
         best_f1, best_f1_threshold = metrics.best_f1_score(self.val_labels, predict_results, self.pos_label)
-        tools.append_to_dict(self.hist, "best_f1", best_f1)
-        tools.append_to_dict(self.hist, "best_f1_threshold", best_f1_threshold)
+        tools.append_to_dict(self.metrics, "best_f1", best_f1)
+        tools.append_to_dict(self.metrics, "best_f1_threshold", best_f1_threshold)
