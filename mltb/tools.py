d1f4bfc77e7e2af2105ae5b73b36e160b427ea9b
Philip May <eniak.info@gmail.com>
2018-11-27 18:51:54 +0100

    docstring multi_param_call

--- mltb/tools.py
+++ mltb/tools.py
@@ -1,8 +1,22 @@
-def multi_param_call(function, param_dict, iterations):
+def multi_param_call(function, param_dict, iterations, verbose=1):
+    """Call function multiple times and return dict with resultsself.
+
+    Parameters
+    ----------
+    function
+        The function to callself.
+    param_dict : dictionary
+        Dictionary with params that are used to call the function.
+    iterations : int
+        Number of iterations that function will be called.
+    verbose : int, optional
+        If 1 status will be print, else not.
+    """
     result = {}
     for key, value in param_dict.items():
         for i in range(iterations):
             f_result = function(value)
             result.setdefault(key, []).append(f_result)
-            print("Done with %s - iteration %i of %i." % (key, i+1, iterations))
+            if verbose == 1:
+                print("Done with %s - iteration %i of %i." % (key, i+1, iterations))
     return result
