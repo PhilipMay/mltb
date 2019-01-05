adc5a76cf9f3efb8aeb7733073592bc85a91977c
PhilipMay <eniak.info@gmail.com>
2019-01-05 14:44:52 +0100

    improved build_pypi script

--- build_pypi.sh
+++ build_pypi.sh
@@ -1,3 +1,5 @@
+echo Please make sure to update the version number in setup.py and __init__.py
+read -p "Press enter to continue"
 rm -r dist
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
