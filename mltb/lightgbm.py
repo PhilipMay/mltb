c70a7bc99c81f0bbe4dc1ed0ead80dcaa58ab6c6
PhilipMay <eniak.info@gmail.com>
2019-05-30 10:23:53 +0200

    Lightgbm binary class metric for f1 and acc

--- mltb/lightgbm.py
+++ mltb/lightgbm.py
@@ -3,6 +3,7 @@
 from sklearn.metrics import f1_score, accuracy_score
 import numpy as np
 
+
 def multi_class_f1_score_factory(num_classes, average):
     """Factory for LightGBM multi class F1-score function.
 
@@ -37,6 +38,37 @@ def multi_class_f1_score_factory(num_classes, average):
 
     return multi_class_f1_score
 
+
+def binary_class_f1_score(y_pred, data):
+    """LightGBM binary class F1-score function.
+
+    Parameters
+    ----------
+    y_pred
+        LightGBM predictions.
+    data
+        LightGBM ``'Dataset'``.
+
+    Returns
+    -------
+    (eval_name, eval_result, is_higher_better)
+        ``'eval_name'`` :
+            is always 'f1' - the name of the metric
+        ``'eval_result'`` : float
+            is the result of the metric
+         ``'is_higher_better'`` : bool
+            is always 'True' because higher F1 score is better
+
+    See Also
+    --------
+    * `sklearn.metrics.f1_score: <https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html>`
+    * `LightGBM Training API: <https://lightgbm.readthedocs.io/en/latest/Python-API.html#training-api>`
+    """
+    y_true = data.get_label()
+    y_pred = np.round(y_pred)
+    return 'f1', f1_score(y_true, y_pred), True
+
+
 def multi_class_accuracy_score_factory(num_classes):
     """Factory for LightGBM multi class accuracy-score function.
 
@@ -59,3 +91,33 @@ def multi_class_accuracy_score_factory(num_classes):
         return 'accuracy', accuracy_score(y_true, y_pred), True
 
     return multi_class_accuracy_score
+
+
+def binary_class_accuracy_score(y_pred, data):
+    """LightGBM binary class accuracy-score function.
+
+    Parameters
+    ----------
+    y_pred
+        LightGBM predictions.
+    data
+        LightGBM ``'Dataset'``.
+
+    Returns
+    -------
+    (eval_name, eval_result, is_higher_better)
+        ``'eval_name'`` :
+            is always 'accuracy' - the name of the metric
+        ``'eval_result'`` : float
+            is the result of the metric
+         ``'is_higher_better'`` : bool
+            is always 'True' because higher accuracy score is better
+
+    See Also
+    --------
+    * `sklearn.metrics.accuracy_score: <https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html>`
+    * `LightGBM Training API: <https://lightgbm.readthedocs.io/en/latest/Python-API.html#training-api>`
+    """
+    y_true = data.get_label()
+    y_pred = np.round(y_pred)
+    return 'accuracy', accuracy_score(y_true, y_pred), True
