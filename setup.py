7e5ce3389a0b1f4c33fe2f9e1a3296e7e857bab3
PhilipMay <eniak.info@gmail.com>
2019-06-01 00:20:12 +0200

    Version 0.2.dev1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mltb",
-    version="0.1",
+    version="0.2.dev1",
     maintainer="Philip May",
     author="Philip May",
     author_email="pm@eniak.de",
