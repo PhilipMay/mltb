7fce1f77294dccf94f6d4c65b2edd058a654617b
PhilipMay <eniak.info@gmail.com>
2019-09-16 16:40:28 +0200

    tensorflow package with mem growth

--- mltb/__init__.py
+++ mltb/__init__.py
@@ -2,5 +2,6 @@ from . import metrics
 from . import plot
 from . import tools
 from . import lightgbm
+from . import tensorflow
 
 __version__ = '0.2.dev1'
---
+++ mltb/tensorflow.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+def set_gpu_mem_growth():
+    """
+    Only grow the memory usage as is needed by the process
+
+    See Also
+    --------
+    * `Limiting GPU memory growth <https://www.tensorflow.org/beta/guide/using_gpu#limiting_gpu_memory_growth>`_
+    """
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
