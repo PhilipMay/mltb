f7ae223452a7d074a3ef8fe75dfc5659096afa3b
PhilipMay <eniak.info@gmail.com>
2019-12-03 15:59:06 +0100

    tensorflow: added set_max_gpu_mem

--- mltb/tensorflow.py
+++ mltb/tensorflow.py
@@ -17,3 +17,27 @@ def set_gpu_mem_growth():
             tf.config.experimental.set_memory_growth(gpu, True)
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+
+
+def set_max_gpu_mem(memory_limit):
+    """
+    Set a hard limit on the total memory to allocate on the GPU.
+
+    This code is for tensorflow 2. It does not work with tensorflow 1.
+
+    Parameters
+    ----------
+    memory_limit
+        Maximum memory (in MB) to allocate on the virtual device. Currently only supported for GPUs.
+
+    See Also
+    --------
+    * `Limiting GPU memory growth <https://www.tensorflow.org/beta/guide/using_gpu#limiting_gpu_memory_growth>`_
+    """
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        tf.config.experimental.set_virtual_device_configuration(
+                gpus[0],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
