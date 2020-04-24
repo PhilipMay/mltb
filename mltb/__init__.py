46141be8694353dd2d4c9095af935d3887822a43
PhilipMay <eniak.info@gmail.com>
2020-04-24 18:03:16 +0200

    to version 0.5.dev1

--- mltb/__init__.py
+++ mltb/__init__.py
@@ -6,4 +6,4 @@ from . import plot
 from . import shap
 from . import tools
 
-__version__ = '0.5'
+__version__ = '0.5.dev1'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mltb",
-    version="0.5",
+    version="0.5.dev1",
     maintainer="Philip May",
     author="Philip May",
     author_email="pm@eniak.de",
