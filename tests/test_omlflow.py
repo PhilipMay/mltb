9691e9b53eed985592156a22b9adf1c804253133
PhilipMay <eniak.info@gmail.com>
2021-03-08 22:18:49 +0100

    fix omlflow test

--- tests/test_omlflow.py
+++ tests/test_omlflow.py
@@ -5,15 +5,15 @@ from mlflow.tracking import MlflowClient
 from mltb.omlflow import OptunaMLflow
 
 
-def _objective_func_factory(tracking_uri):
+def _objective_func_factory(tracking_uri, num_folds):
     def _objective_func(trial):
         with OptunaMLflow(trial, tracking_uri) as om:
             x = trial.suggest_uniform("x", -10, 10)
             om.log_param("x", x)
             results = []
 
-            # do 3 folds
-            for i in range(3):
+            # do folds
+            for i in range(num_folds):
                 result = (x - 2) ** 2
                 om.log_iter({"x": result}, i)
                 results.append(result)
@@ -29,9 +29,10 @@ def test_study_name(tmpdir):
     tracking_file_name = "file:{}".format(tmpdir)
     study_name = "my_study"
     n_trials = 2
+    num_folds = 3
 
     study = optuna.create_study(study_name=study_name)
-    study.optimize(_objective_func_factory(tracking_file_name), n_trials=n_trials)
+    study.optimize(_objective_func_factory(tracking_file_name, num_folds), n_trials=n_trials)
 
     mlfl_client = MlflowClient(tracking_file_name)
     experiments = mlfl_client.list_experiments()
@@ -42,4 +43,4 @@ def test_study_name(tmpdir):
     experiment_id = experiment.experiment_id
 
     run_infos = mlfl_client.list_run_infos(experiment_id)
-    assert len(run_infos) == n_trials
+    assert len(run_infos) == n_trials + n_trials * num_folds
