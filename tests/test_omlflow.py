5f4650b7b5cf326d624e665bcc4d03f563942d59
PhilipMay <eniak.info@gmail.com>
2021-03-08 21:50:13 +0100

    fix omlflow test

--- tests/test_omlflow.py
+++ tests/test_omlflow.py
@@ -5,21 +5,24 @@ from mlflow.tracking import MlflowClient
 from mltb.omlflow import OptunaMLflow
 
 
-def _objective_func(trial, tracking_uri):
-    with OptunaMLflow(trial, tracking_uri) as om:
-        x = trial.suggest_uniform("x", -10, 10)
-        om.log_param("x", x)
-        results = []
+def _objective_func_factory(tracking_uri):
+    def _objective_func(trial):
+        with OptunaMLflow(trial, tracking_uri) as om:
+            x = trial.suggest_uniform("x", -10, 10)
+            om.log_param("x", x)
+            results = []
 
-        # do 3 folds
-        for i in range(3):
-            result = (x - 2) ** 2
-            om.log_iter({"x": result}, i)
-            results.append(result)
+            # do 3 folds
+            for i in range(3):
+                result = (x - 2) ** 2
+                om.log_iter({"x": result}, i)
+                results.append(result)
 
-        result = np.mean(results)
-        om.log_metric("result", result)
-        return result
+            result = np.mean(results)
+            om.log_metric("result", result)
+            return result
+
+    return _objective_func
 
 
 def test_study_name(tmpdir):
@@ -28,7 +31,7 @@ def test_study_name(tmpdir):
     n_trials = 2
 
     study = optuna.create_study(study_name=study_name)
-    study.optimize(_objective_func, n_trials=n_trials)
+    study.optimize(_objective_func_factory(tracking_file_name), n_trials=n_trials)
 
     mlfl_client = MlflowClient(tracking_file_name)
     experiments = mlfl_client.list_experiments()
