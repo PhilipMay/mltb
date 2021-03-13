9e9d7190cc39b478801be4e95a30ecb3df43de3a
PhilipMay <eniak.info@gmail.com>
2021-03-13 13:05:36 +0100

    use decorator

--- demo/omlflow_demo.py
+++ demo/omlflow_demo.py
@@ -4,19 +4,17 @@ import numpy as np
 from mltb.omlflow import OptunaMLflow
 
 
-def objective(trial):
-    with OptunaMLflow(trial, "", num_name_digits=5, enforce_clean_git=True) as om:
-        results = []
-        for i in range(3):
-            x = trial.suggest_uniform("x", -10, 10)
-            om.log_param("x", x)
-            result = (x - 2) ** 2
-            om.log_iter({"x": result}, i)
-            results.append(result)
-        result = np.mean(results)
-        om.log_metric("result", result)
-        time.sleep(2)
-        return result
+@OptunaMLflow()
+def objective(omlflow):
+    results = []
+    for i in range(3):
+        x = omlflow.suggest_uniform("x", -10, 10)
+        result = (x - 2) ** 2
+        omlflow.log_iter({"x": result}, i)
+        results.append(result)
+    result = np.mean(results)
+    time.sleep(2)
+    return result
 
 
 study = optuna.create_study(study_name="_test_mlflow_context_namager_01")
--- mltb/omlflow.py
+++ mltb/omlflow.py
@@ -6,6 +6,8 @@ import traceback
 import warnings
 import git
 import os
+from functools import wraps
+import sys
 
 import mlflow
 from mlflow.entities import RunStatus
@@ -18,11 +20,16 @@ _logger = logging.getLogger(__name__)
 class OptunaMLflow(object):
     """Wrapper to log to Optuna and MLflow at the same time."""
 
-    def __init__(self, trial, tracking_uri=None, num_name_digits=3, enforce_clean_git=False):
+    def __init__(
+        self,
+        tracking_uri=None,
+        num_name_digits=3,
+        enforce_clean_git=False,
+        optuna_result_name="optuna_result"
+    ):
         """Init class.
 
         Args:
-            trial ([``optuna.trial.Trial``]): The optuna trial to use.
             tracking_uri ([str], optional): See `MLflow documentation
                 <https://www.mlflow.org/docs/latest/python_api/mlflow.html#mlflow.set_tracking_uri>`_.
                 Defaults to ``None`` which logs to the default locale folder ``./mlruns`` or
@@ -32,7 +39,6 @@ class OptunaMLflow(object):
                 uncommited changes. Defaults to False. Also see `git.repo.base.Repo.is_dirty
                 <https://gitpython.readthedocs.io/en/stable/reference.html#git.repo.base.Repo.is_dirty>`_
         """
-        self._trial = trial
         self._tracking_uri = tracking_uri
         self._num_name_digits = num_name_digits
         self._enforce_clean_git = enforce_clean_git
@@ -40,17 +46,90 @@ class OptunaMLflow(object):
         self._iter_metrics = {}
         self._next_iter_num = 0
         self._hostname = None
+        self._optuna_result_name = optuna_result_name
+
+    def __call__(self, func):
+
+        @wraps(func)
+        def objective_decorator(trial):
+            """Decorator for optuna objective function.
+
+            Args:
+                trial ([``optuna.trial.Trial``]): The optuna trial to use.
+            """
+            self._trial = trial
+
+            # check if GIT repo is clean
+            if self._enforce_clean_git:
+                self._check_repo_is_dirty()
+
+            try:
+                # set tracking_uri for MLflow
+                if self._tracking_uri is not None:
+                    mlflow.set_tracking_uri(self._tracking_uri)
+
+                mlflow.set_experiment(self._trial.study.study_name)
+
+                digits_format_string = "{{:0{}d}}".format(self._num_name_digits)
+                mlflow.start_run(run_name=digits_format_string.format(self._trial.number))
+                _logger.info("Run {} started.".format(self._trial.number))
+
+                # TODO: use set_tags with dict
+                self.set_tag("hostname", self._get_hostname())
+                self.set_tag("process_id", os.getpid())
+            except Exception as e:
+                _logger.error(
+                    "Exception raised during MLflow communication! Exception: {}".format(e),
+                    exc_info=True,
+                )
+
+            try:
+                # call objective function
+                result = func(self)
+
+                # log the result to MLflow but not optuna
+                self.log_metric(self._optuna_result_name, result, optuna_log=False)
+
+                # extract tags from trial
+                tags = {}
+                # Set direction and convert it to str and remove the common prefix.
+                study_direction = self._trial.study.direction
+                if isinstance(study_direction, optuna._study_direction.StudyDirection):
+                    tags["direction"] = str(study_direction).split(".")[-1]
+
+                distributions = {(k + "_distribution"): str(v) for (k, v) in self._trial.distributions.items()}
+                tags.update(distributions)
+                self.set_tags(tags, optuna_log=False)
+                self._end_run("FINISHED")
+
+                return result
+            except:  # noqa: E722
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                exc_text = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+                self.set_tag("exception", exc_text)
+                if exc_type is KeyboardInterrupt:
+                    self._end_run(RunStatus.to_string(RunStatus.KILLED))
+                else:
+                    self._end_run(RunStatus.to_string(RunStatus.FAILED))
+                raise  # raise exception again
+
+        return objective_decorator
 
     #####################################
     # MLflow wrapper functions
     #####################################
 
-    def log_metric(self, key, value, step=None):
+    def log_metric(self, key, value, step=None, optuna_log=True):
         """Wrapper of the corresponding MLflow function.
 
         The data is also added to Optuna as an user attribute.
+
+        Args:
+            optuna_log (bool, optional): Internal parameter that should be ignored by the API user.
+                Defaults to True.
         """
-        self._trial.set_user_attr(key, value)
+        if optuna_log:
+            self._trial.set_user_attr(key, value)
         _logger.info(f"Metric: {key}: {value} at step: {step}")
         try:
             mlflow.log_metric(key, value, step=None)
@@ -228,59 +307,6 @@ class OptunaMLflow(object):
             error_message = "Git repository '{}' is dirty!".format(path)
             _logger.error(error_message)
             raise RuntimeError(error_message)
-
-    #####################################
-    # context manager functions
-    #####################################
-
-    def __enter__(self):
-        # check if GIT repo is clean
-        if self._enforce_clean_git:
-            self._check_repo_is_dirty()
-
-        try:
-            # set tracking_uri for MLflow
-            if self._tracking_uri is not None:
-                mlflow.set_tracking_uri(self._tracking_uri)
-
-            mlflow.set_experiment(self._trial.study.study_name)
-
-            digits_format_string = "{{:0{}d}}".format(self._num_name_digits)
-            mlflow.start_run(run_name=digits_format_string.format(self._trial.number))
-            _logger.info("Run {} started.".format(self._trial.number))
-
-            # TODO: use set_tags with dict
-            self.set_tag("hostname", self._get_hostname())
-            self.set_tag("process_id", os.getpid())
-        except Exception as e:
-            _logger.error(
-                "Exception raised during MLflow communication! Exception: {}".format(e),
-                exc_info=True,
-            )
-        return self
-
-    def __exit__(self, exc_type, exc_value, tb):
-        if exc_type is None:  # no exception
-
-            # extract tags from trial
-            tags = {}
-            # Set direction and convert it to str and remove the common prefix.
-            study_direction = self._trial.study.direction
-            if isinstance(study_direction, optuna._study_direction.StudyDirection):
-                tags["direction"] = str(study_direction).split(".")[-1]
-
-            distributions = {(k + "_distribution"): str(v) for (k, v) in self._trial.distributions.items()}
-            tags.update(distributions)
-            self.set_tags(tags, optuna_log=False)
-            self._end_run("FINISHED")
-        else:  # exception
-            exc_text = "".join(traceback.format_exception(exc_type, exc_value, tb))
-            self.set_tag("exception", exc_text)
-            if exc_type is KeyboardInterrupt:
-                self._end_run(RunStatus.to_string(RunStatus.KILLED))
-            else:
-                self._end_run(RunStatus.to_string(RunStatus.FAILED))
-            return False  # raise exception again
 
     #####################################
     # Optuna wrapper functions
